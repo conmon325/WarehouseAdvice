d7af0f811f55f54ac7277cd954050941b51cf863
Montagano <Connor.Montagano@nmrk.com>
2023-12-20 11:00:26 -0500

    map update 12/20/23

--- src/components/App.js
+++ src/components/App.js
@@ -219,7 +219,7 @@ function App() {
 					<Row className='flex m-3'>
 						<h2 className='text-center p-3'>Market Insights</h2>
 						<Col md={5} lg={4} xl={5} xxl={4} className='text-center'>
-							<iframe src="//conmon.maps.arcgis.com/apps/Embed/index.html?webmap=564443b4886e4d65b2d6a87872057e4c&extent=-77.1948,38.9331,-70.7184,41.4215&zoom=true&previewImage=false&scale=true&disable_scroll=true&theme=light" height="800" width="1200"></iframe>
+							<iframe src="//conmon.maps.arcgis.com/apps/Embed/index.html?webmap=8a8314f7f22a4a20bbabcdf906c7800d&extent=-76.2032,39.4097,-72.1465,41.1053&zoom=true&previewImage=false&scale=true&disable_scroll=true&theme=light" height="800" width="1200"></iframe>
 						</Col>
 						<Col md={5} lg={4} xl={5} xxl={4}>
 							{isError ? (
